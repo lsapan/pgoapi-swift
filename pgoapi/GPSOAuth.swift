c7d0fd621d92ef99a55f6cc6aaa69fd3c33c6441
Rowell Heria <owieh@me.com>
2016-08-02 13:12:49 +0100

    added new endpoint and updated Api struct

---
+++ pgoapi/GPSOAuth.swift
@@ -0,0 +1,9 @@
+//
+//  GPSOAuth.swift
+//  pgoapi
+//
+//  Created by Rowell Heria on 02/08/2016.
+//  Copyright © 2016 Coadstal. All rights reserved.
+//
+
+import Foundation
--- pgoapi/Globals.swift
+++ pgoapi/Globals.swift
@@ -12,13 +12,23 @@ struct Endpoint {
     static let LoginInfo = "https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize"
     static let LoginTicket = "https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize"
     static let LoginOAuth = "https://sso.pokemon.com/sso/oauth2.0/accessToken"
+    static var LoginProvider = ""
     static let Rpc = "https://pgorelease.nianticlabs.com/plfe/rpc"
+    static let GoogleLogin = "https://android.clients.google.com/auth"
+}
+
+struct Location {
+    static var lat:Double = 0
+    static var long:Double = 0
+    static var alt:Double = 0
 }
 
 struct Api {
     static var endpoint = Endpoint.Rpc
     static let id: UInt64 = 8145806132888207460
     static let SettingsHash = "05daf51635c82611d1aac95c0b051d3ec088a930"
+    static var receivedToken = false
+    static var authToken: Pogoprotos.Networking.Envelopes.AuthTicket = Pogoprotos.Networking.Envelopes.AuthTicket()
 }
 
 enum ApiIntent {
