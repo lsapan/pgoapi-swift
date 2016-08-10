6bb18d2b4110e6d17401990aa43f7e07b7aa6796
PokemonGoSucks <george.swan94@gmail.com>
2016-08-10 18:59:27 -0400

    Update and rename RpcApi.swift to PGoRpcApi.swift

--- PGoApi/Classes/PGoRpcApi.swift
+++ PGoApi/Classes/PGoRpcApi.swift
@@ -15,18 +15,16 @@ import ProtocolBuffers
 
 class PGoRpcApi {
     let intent: PGoApiIntent
-    let auth: PGoAuth
+    var auth: PGoAuth
     let delegate: PGoApiDelegate?
     let subrequests: [PGoApiMethod]
     let api: PGoApiRequest
     let encrypt: PGoEncrypt
     private var timeSinceStart:UInt64 = 0
     private var locationHex: NSData
-    private var requestId: UInt64 = 8145806132888207460
+    private var requestId: UInt64 = 0
     
-    init(subrequests: [PGoApiMethod], intent: PGoApiIntent, auth: PGoAuth, api: PGoApiRequest, delegate: PGoApiDelegate?) {
-        // TODO: Eventually use a custom session
-        // Add "Niantic App" as the User-Agent
+    internal init(subrequests: [PGoApiMethod], intent: PGoApiIntent, auth: PGoAuth, api: PGoApiRequest, delegate: PGoApiDelegate?) {
         let manager = Manager.sharedInstance
         manager.session.configuration.HTTPAdditionalHeaders = [
             "User-Agent": "Niantic App"
@@ -43,8 +41,7 @@ class PGoRpcApi {
         self.requestId = randomUInt64(UInt64(pow(Double(2),Double(62))), max: UInt64(pow(Double(2),Double(63))))
     }
     
-    func request() {
-        // TODO: Eventually update this function to take playerPosition, and pass to buildMainRequest
+    internal func request() {
         let requestData = buildMainRequest().data()
         
         Alamofire.request(.POST, auth.endpoint, parameters: [:], encoding: .Custom({
@@ -66,7 +63,6 @@ class PGoRpcApi {
     }
     
     private func locationToHex(value:Double) -> NSData {
-        
         let f2d = String(unsafeBitCast(value, UInt64.self), radix: 16)
         let s = f2d.uppercaseString.stringByReplacingOccurrencesOfString(" ", withString: "")
         
@@ -75,10 +71,7 @@ class PGoRpcApi {
         for i in 0.stride(to:s.characters.count, by:2) {
             var x = -(s.characters.count) + i + 2
             if x > s.characters.count - 2 {
-                x = s.characters.count - x
-                if (x < 0) {
-                    x = 0
-                }
+                x = s.characters.count
             }
             let r = s.startIndex.advancedBy(i)..<s.endIndex.advancedBy(x)
             let substring = s[r]
@@ -86,23 +79,17 @@ class PGoRpcApi {
             let byte = UInt8(substring.withCString { strtoul($0, nil, 16) })
             data?.appendBytes([byte] as [UInt8], length: 1)
         }
-        
         return data!
     }
-
+    
     private func hashAuthTicket() -> UInt32 {
-        let xxh32:xxhash = xxhash(seed: 0x1B845238)
-        xxh32.update(Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(auth.authToken!.data().bytes), count: auth.authToken!.data().length)))
-        let firstHash = xxh32.digest()
-        
-        let xxh32_2:xxhash = xxhash(seed: firstHash)
-        
-        xxh32_2.update(Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(self.locationHex.bytes), count: self.locationHex.length)))
-        return xxh32_2.digest()
+        let xxh32:xxhash = xxhash()
+        let firstHash = xxh32.xxh32(0x1B845238, input: auth.authToken!.data().getUInt8Array())
+        return xxh32.xxh32(firstHash, input: self.locationHex.getUInt8Array())
     }
     
     private func hashLocation() -> UInt32 {
-        let xxh32:xxhash = xxhash(seed: 0x1B845238)
+        let xxh32:xxhash = xxhash()
         
         let LocationData = NSMutableData()
         LocationData.appendData(locationToHex(self.api.Location.lat))
@@ -112,11 +99,9 @@ class PGoRpcApi {
         } else {
             LocationData.appendData(locationToHex(self.api.Location.alt!))
         }
-        
         self.locationHex = LocationData
         
-        xxh32.update(Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(LocationData.bytes), count: LocationData.length)))
-        return xxh32.digest()
+        return xxh32.xxh32(0x1B845238, input: LocationData.getUInt8Array())
     }
     
     private func randomUInt64(min: UInt64, max: UInt64) -> UInt64 {
@@ -124,25 +109,39 @@ class PGoRpcApi {
     }
     
     private func hashRequest(requestData:NSData) -> UInt64 {
-        let xxh64:xxhash = xxhash(seed: 0x1B845238)
-        let firstHash = xxh64.xxh64(0x1B845238, input: Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(auth.authToken!.data().bytes), count: auth.authToken!.data().length)))
-        return xxh64.xxh64(firstHash, input: Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(requestData.bytes), count: requestData.length)))
+        let xxh64:xxhash = xxhash()
+        let firstHash = xxh64.xxh64(0x1B845238, input: auth.authToken!.data().getUInt8Array())
+        return xxh64.xxh64(firstHash, input: requestData.getUInt8Array())
     }
     
     private func buildMainRequest() -> Pogoprotos.Networking.Envelopes.RequestEnvelope {
-        
         print("Generating main request...")
         
         let requestBuilder = Pogoprotos.Networking.Envelopes.RequestEnvelope.Builder()
         requestBuilder.statusCode = 2
         requestBuilder.requestId = self.requestId
-        print(self.requestId)
         requestBuilder.unknown12 = 989
         
         requestBuilder.latitude = self.api.Location.lat
         requestBuilder.longitude = self.api.Location.long
         if self.api.Location.alt != nil {
             requestBuilder.altitude = self.api.Location.alt!
+        }
+        
+        print("Generating subrequests...")
+        let signatureBuilder = Pogoprotos.Networking.Envelopes.Signature.Builder()
+        
+        for subrequest in subrequests {
+            print("Processing \(subrequest)...")
+            let subrequestBuilder = Pogoprotos.Networking.Requests.Request.Builder()
+            subrequestBuilder.requestType = subrequest.id
+            subrequestBuilder.requestMessage = subrequest.message.data()
+            let subData = try! subrequestBuilder.build()
+            requestBuilder.requests += [subData]
+            if auth.authToken != nil {
+                let h64 = hashRequest(subData.data())
+                signatureBuilder.requestHash.append(h64)
+            }
         }
         
         if (auth.authToken == nil) {
@@ -154,18 +153,11 @@ class PGoRpcApi {
         } else {
             requestBuilder.authTicket = auth.authToken!
             
-            let signatureBuilder = Pogoprotos.Networking.Envelopes.Signature.Builder()
             signatureBuilder.locationHash2 = hashLocation()
             signatureBuilder.locationHash1 = hashAuthTicket()
-            for request in requestBuilder.requests {
-                let h64 = hashRequest(request.data())
-                signatureBuilder.requestHash.append(h64)
-            }
-            
             signatureBuilder.unknown22 = self.encrypt.randomBytes()
-            
             signatureBuilder.timestamp = UInt64(NSDate().timeIntervalSince1970 * 1000.0)
-            signatureBuilder.timestampSinceStart = timeSinceStart
+            signatureBuilder.timestampSinceStart = UInt64(NSDate().timeIntervalSince1970 * 1000.0) - timeSinceStart
             
             let signature = try! signatureBuilder.build()
             
@@ -173,17 +165,7 @@ class PGoRpcApi {
             unknown6.requestType = 6
             
             let unknown2 = requestBuilder.getUnknown6Builder().getUnknown2Builder()
-            
             unknown2.encryptedSignature = self.encrypt.encryptUsingLib(signature.data())
-        }
-        
-        print("Generating subrequests...")
-        for subrequest in subrequests {
-            print("Processing \(subrequest)...")
-            let subrequestBuilder = Pogoprotos.Networking.Requests.Request.Builder()
-            subrequestBuilder.requestType = subrequest.id
-            subrequestBuilder.requestMessage = subrequest.message.data()
-            requestBuilder.requests += [try! subrequestBuilder.build()]
         }
         
         self.requestId += 1
@@ -197,6 +179,11 @@ class PGoRpcApi {
         
         let response = try! Pogoprotos.Networking.Envelopes.ResponseEnvelope.parseFromData(data)
         let subresponses = parseSubResponses(response)
+        if auth.authToken == nil {
+            if response.hasAuthTicket {
+                auth.authToken = response.authTicket
+            }
+        }
         return PGoApiResponse(response: response, subresponses: subresponses)
     }
     
