bd0490e5c57a1c8a70117f5f7ccfe856731b2a7f
PokemonGoSucks <george.swan94@gmail.com>
2016-09-01 00:59:50 -0400

    Cleaning up utils

--- PGoApi/Classes/PGoUtils.swift
+++ PGoApi/Classes/PGoUtils.swift
@@ -21,29 +21,26 @@ public class PGoLocationUtils {
     
     public init () {}
     
-    public struct Coordinates {
+    public struct PGoCoordinate {
         public var latitude: Double?
         public var longitude: Double?
-    }
-    
-    public struct movedCoordinates {
+        public var altitude: Double?
         public var distance: Double?
-        public var Coordinates: PGoLocationUtils.Coordinates?
-        public var movedDistance: Double?
+        public var displacement: Double?
     }
     
-    public func reverseGeocode(location: String, completionHandler: (PGoLocationUtils.Coordinates?) -> ()) {
+    public func reverseGeocode(location: String, completionHandler: (PGoLocationUtils.PGoCoordinate?) -> ()) {
         /*
          
          Example func for completionHandler: func receivedGeocode(results:PGoLocationUtils.Coordinates?)
          
         */
         
-        var result: Coordinates?
+        var result: PGoCoordinate?
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(location, completionHandler: {(placeData: [CLPlacemark]?, error: NSError?) -> Void in
             if (placeData?.count > 0) {
-                result = Coordinates(latitude: (placeData![0].location?.coordinate.latitude)!, longitude: (placeData![0].location?.coordinate.longitude)!)
+                result = PGoCoordinate(latitude: (placeData![0].location?.coordinate.latitude)!, longitude: (placeData![0].location?.coordinate.longitude)!, altitude: placeData![0].location?.altitude, distance: 0, displacement: 0)
                 completionHandler(result)
             }
         })
@@ -65,7 +62,7 @@ public class PGoLocationUtils {
         return distance
     }
     
-    public func moveDistanceToPoint(startLatitude:Double, startLongitude:Double, endLatitude:Double, endLongitude: Double, distance: Double, unitOfDistance: PGoLocationUtils.unit? = .Meters) -> PGoLocationUtils.movedCoordinates {
+    public func moveDistanceToPoint(startLatitude:Double, startLongitude:Double, endLatitude:Double, endLongitude: Double, distance: Double, unitOfDistance: PGoLocationUtils.unit? = .Meters) -> PGoLocationUtils.PGoCoordinate {
         let maxDistance = getDistanceBetweenPoints(startLatitude, startLongitude: startLongitude, endLatitude: endLatitude, endLongitude: endLongitude)
         
         var distanceConverted = distance
@@ -83,10 +80,10 @@ public class PGoLocationUtils {
             distanceMove = 1
         }
         
-        return movedCoordinates(distance: maxDistance, Coordinates: Coordinates(latitude: startLatitude + ((endLatitude - startLatitude) * distanceMove), longitude: startLongitude + ((endLongitude - startLongitude) * distanceMove)), movedDistance: distanceMove * maxDistance)
+        return PGoCoordinate(latitude: startLatitude + ((endLatitude - startLatitude) * distanceMove), longitude: startLongitude + ((endLongitude - startLongitude) * distanceMove), altitude: 0, distance: maxDistance, displacement: distanceMove * maxDistance)
     }
     
-    public func moveDistanceWithBearing(startLatitude:Double, startLongitude:Double, bearing: Double, distance: Double, bearingUnits: PGoLocationUtils.bearingUnits? = .Radian, unitOfDistance: PGoLocationUtils.unit? = .Meters) -> PGoLocationUtils.Coordinates {
+    public func moveDistanceWithBearing(startLatitude:Double, startLongitude:Double, bearing: Double, distance: Double, bearingUnits: PGoLocationUtils.bearingUnits? = .Radian, unitOfDistance: PGoLocationUtils.unit? = .Meters) -> PGoLocationUtils.PGoCoordinate {
         
         var distanceConverted = distance
         if unitOfDistance == .Miles {
@@ -110,6 +107,6 @@ public class PGoLocationUtils {
         let latitude = (asin(sin(lat) * cos(distanceRadian) + cos(lat) * sin(distanceRadian) * cos(bearingConverted))) * 180 / M_PI
         let longitude = (long + atan2(sin(bearingConverted) * sin(distanceRadian) * cos(lat), cos(distanceRadian) - sin(lat) * sin(latitude))) * 180 / M_PI
         
-        return Coordinates(latitude: latitude, longitude: longitude)
+        return PGoCoordinate(latitude: latitude, longitude: longitude, altitude: 0, distance: 0, displacement: 0)
     }
 }
