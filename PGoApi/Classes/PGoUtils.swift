c742465f29dc1934c00c936e13ce8d80f0e7dcc4
PokemonGoSucks <george.swan94@gmail.com>
2016-09-04 22:09:47 -0400

    Solving osx framework issue

--- PGoApi/Classes/PGoUtils.swift
+++ PGoApi/Classes/PGoUtils.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import CoreLocation
-import AddressBookUI
+import MapKit
 
 
 public class PGoLocationUtils {
@@ -28,22 +28,45 @@ public class PGoLocationUtils {
         public var altitude: Double?
         public var distance: Double?
         public var displacement: Double?
+        public var address: [NSObject: AnyObject]?
+        public var mapItem: MKMapItem?
     }
     
-    public func geocode(latitude: Double, longitude: Double, completionHandler: (location: String?) -> ()) {
+    public struct PGoDirections {
+        public var coordinates: Array<PGoLocationUtils.PGoCoordinate>
+        public var duration: Double
+    }
+    
+    public func geocode(latitude: Double, longitude: Double, completionHandler: (PGoLocationUtils.PGoCoordinate?) -> ()) {
         /*
          
          Example func for completionHandler:
-         func receivedGeocode(location: String?)
+         func receivedGeocode(results:PGoLocationUtils.PGoCoordinate?)
          
          */
         
         let location = CLLocation(latitude: latitude, longitude: longitude)
-        CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error) -> Void in
-            if placemarks?.count > 0 {
-                let addressDictionary = placemarks![0]
-                let address = ABCreateStringWithAddressDictionary(addressDictionary.addressDictionary!, false)
-                completionHandler(location: address.stringByReplacingOccurrencesOfString("\n", withString: ", "))
+        CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placeData, error) -> Void in
+            if placeData?.count > 0 {
+                let addressDictionary = placeData![0]
+                let address = addressDictionary.addressDictionary!
+                let result = PGoCoordinate(
+                    latitude: latitude,
+                    longitude: longitude,
+                    altitude: nil,
+                    distance: nil,
+                    displacement: nil,
+                    address: address,
+                    mapItem: MKMapItem(
+                        placemark: MKPlacemark(
+                            coordinate: placeData![0].location!.coordinate,
+                            addressDictionary: placeData![0].addressDictionary as! [String:AnyObject]?
+                        )
+                    )
+                )
+                completionHandler(result)
+            } else {
+                completionHandler(nil)
             }
         })
     }
@@ -60,8 +83,24 @@ public class PGoLocationUtils {
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(location, completionHandler: {(placeData: [CLPlacemark]?, error: NSError?) -> Void in
             if (placeData?.count > 0) {
-                result = PGoCoordinate(latitude: (placeData![0].location?.coordinate.latitude)!, longitude: (placeData![0].location?.coordinate.longitude)!, altitude: placeData![0].location?.altitude, distance: 0, displacement: 0)
+                let addressDictionary = placeData![0]
+                result = PGoCoordinate(
+                    latitude: (placeData![0].location?.coordinate.latitude)!,
+                    longitude: (placeData![0].location?.coordinate.longitude)!,
+                    altitude: placeData![0].location?.altitude,
+                    distance: nil,
+                    displacement: nil,
+                    address: addressDictionary.addressDictionary!,
+                    mapItem: MKMapItem(
+                        placemark: MKPlacemark(
+                            coordinate: placeData![0].location!.coordinate,
+                            addressDictionary: placeData![0].addressDictionary as! [String:AnyObject]?
+                        )
+                    )
+                )
                 completionHandler(result)
+            } else {
+                completionHandler(nil)
             }
         })
     }
@@ -100,7 +139,15 @@ public class PGoLocationUtils {
             distanceMove = 1
         }
         
-        return PGoCoordinate(latitude: startLatitude + ((endLatitude - startLatitude) * distanceMove), longitude: startLongitude + ((endLongitude - startLongitude) * distanceMove), altitude: 0, distance: maxDistance, displacement: distanceMove * maxDistance)
+        return PGoCoordinate(
+            latitude: startLatitude + ((endLatitude - startLatitude) * distanceMove),
+            longitude: startLongitude + ((endLongitude - startLongitude) * distanceMove),
+            altitude: nil,
+            distance: maxDistance,
+            displacement: distanceMove * maxDistance,
+            address: nil,
+            mapItem: nil
+        )
     }
     
     public func moveDistanceWithBearing(startLatitude:Double, startLongitude:Double, bearing: Double, distance: Double, bearingUnits: PGoLocationUtils.bearingUnits? = .Radian, unitOfDistance: PGoLocationUtils.unit? = .Meters) -> PGoLocationUtils.PGoCoordinate {
@@ -127,6 +174,74 @@ public class PGoLocationUtils {
         let latitude = (asin(sin(lat) * cos(distanceRadian) + cos(lat) * sin(distanceRadian) * cos(bearingConverted))) * 180 / M_PI
         let longitude = (long + atan2(sin(bearingConverted) * sin(distanceRadian) * cos(lat), cos(distanceRadian) - sin(lat) * sin(latitude))) * 180 / M_PI
         
-        return PGoCoordinate(latitude: latitude, longitude: longitude, altitude: 0, distance: 0, displacement: 0)
+        return PGoCoordinate(
+            latitude: latitude,
+            longitude: longitude,
+            altitude: nil,
+            distance: nil,
+            displacement: nil,
+            address: nil,
+            mapItem: nil
+        )
+    }
+    
+    private func getMKMapItem(lat: Double, long: Double) -> MKMapItem {
+        let sourceLoc2D = CLLocationCoordinate2DMake(lat, long)
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLoc2D, addressDictionary: nil)
+        let source = MKMapItem(placemark: sourcePlacemark)
+        return source
+    }
+    
+    public func getDirectionsFromToLocations(startLatitude:Double, startLongitude:Double, endLatitude:Double, endLongitude: Double, transportType: MKDirectionsTransportType? = .Walking, completionHandler: (PGoLocationUtils.PGoDirections?) -> ()) {
+        
+        /*
+         
+         Example func for completionHandler:
+         func receivedDirections(result: PGoLocationUtils.PGoDirections?)
+         
+         */
+        
+        var result:Array<PGoCoordinate> = []
+        
+        let request: MKDirectionsRequest = MKDirectionsRequest()
+        let start = getMKMapItem(startLatitude, long: startLongitude)
+        let end = getMKMapItem(endLatitude, long: endLongitude)
+        
+        request.source = start
+        request.destination = end
+        request.requestsAlternateRoutes = true
+        request.transportType = transportType!
+        
+        let directions = MKDirections(request: request)
+        directions.calculateDirectionsWithCompletionHandler ({
+            (response: MKDirectionsResponse?, error: NSError?) in
+            if let routeResponse = response?.routes {
+                let fastestRoute: MKRoute =
+                    routeResponse.sort({$0.expectedTravelTime <
+                        $1.expectedTravelTime})[0]
+                
+                for step in fastestRoute.steps {
+                    result.append(
+                        PGoCoordinate(
+                            latitude: step.polyline.coordinate.latitude,
+                            longitude: step.polyline.coordinate.longitude,
+                            altitude: nil,
+                            distance: step.distance,
+                            displacement: nil,
+                            address: nil,
+                            mapItem: nil
+                        )
+                    )
+                }
+                completionHandler(
+                    PGoLocationUtils.PGoDirections(
+                        coordinates: result,
+                        duration: fastestRoute.expectedTravelTime
+                    )
+                )
+            } else {
+                completionHandler(nil)
+            }
+        })
     }
 }
